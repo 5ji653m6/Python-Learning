1afc6773bfd4b657f80bbdd1114730c51f1aac50
TungLinWu <5ji653m6@gmail.com>
2015-11-21 11:40:37 +0800

    example python code using xml.etree to parsing name, attribute, id

---
+++ Script/xml2.py
@@ -0,0 +1,30 @@
+import xml.etree.ElementTree as ET
+
+input = '''
+<stuff>
+    <users>
+        <user x="2">
+            <id>001</id>
+            <name>Chuck</name>
+        </user>
+        <user x="7">
+            <id>009</id>
+            <name>Brent</name>
+        </user>
+	<user x="agent">
+	    <id>007</id>
+	    <name>James Bond</name>
+	</user>
+    </users>
+</stuff>'''
+
+stuff = ET.fromstring(input)
+lst = stuff.findall('users/user') #users/user is the path of XML content, and output the result as a list format
+print 'User count:', len(lst) #count how many users are there within the <stuff> block
+# as lst is list format we could go through the lst to print out the name we find
+
+for item in lst:
+    print 'Name', item.find('name').text
+    print 'Id', item.find('id').text
+    print 'Attribute', item.get("x"), '\n'
+
