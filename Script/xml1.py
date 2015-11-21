2e26a026273919d60982b0462c54031dc34dcd05
TungLinWu <5ji653m6@gmail.com>
2015-11-21 11:42:03 +0800

    example python code using xml.etree parsing the xml content

--- Script/xml1.py
+++ Script/xml1.py
@@ -1,3 +1,5 @@
+# xml.etree.ElementTree is the library dealing with contract between python and XML
+# https://docs.python.org/2/library/xml.etree.elementtree.html
 import xml.etree.ElementTree as ET
 
 #The ''' triple quote is the start of the XML paragraph, and it also ends with '''
@@ -10,6 +12,7 @@ data = '''
    <email hide="yes"/>
 </person>'''
 
+#ET.fromstring is the deserialization for parsing the XML paragragh into what we are looing for
 tree = ET.fromstring(data)
 print 'Name:',tree.find('name').text
 print 'Attr:',tree.find('email').get('hide')
